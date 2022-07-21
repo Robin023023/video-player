3fc4bc80f0494b0e1e5d05e6d224619883a5f3f4
Robin Hossain <023023robin@gmail.com>
2022-07-21 22:07:41 +0600

    first commit

---
+++ src/VideoPlayer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import ReactPlayer from 'react-player'
+
+export default function VideoPlayer() {
+  return (
+    <div>
+       {/* <ReactPlayer controls url='https://www.youtube.com/watch?v=ysz5S6PUM-U' /> */}
+
+       <ReactPlayer controls url="video/lol1.mp4" />
+
+       
+    </div>
+  )
+}
